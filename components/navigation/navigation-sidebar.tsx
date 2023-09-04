2e55f4039c91227119ccca8261ca55486f2150ac
zoreg <ashutosh.routray126@gmail.com>
2023-09-04 16:16:42 +0530

    local to repo

---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,15 @@
+import { currentProfile } from "@/lib/current-profile";
+import { redirect } from "next/dist/server/api-utils";
+
+export const NavigationSidebar = async() => {
+
+    const profile = await currentProfile();
+
+    if(!profile)return redirect
+
+    return ( 
+        <div>
+            navigation sidebar
+        </div>
+     );
+}
