bb1c944b060b621a41c16f499667ae5cb232511b
zoreg <ashutosh.routray126@gmail.com>
2023-09-04 20:14:49 +0530

    git ignore error fix 1

--- discord-clone/components/navigation/navigation-sidebar.tsx
+++ discord-clone/components/navigation/navigation-sidebar.tsx
@@ -23,7 +23,7 @@ export const NavigationSidebar = async() => {
         <div
             className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3"
         >
-            <NavigationAction/>
+            {/* <NavigationAction/> */}
         </div>
      );
 }
